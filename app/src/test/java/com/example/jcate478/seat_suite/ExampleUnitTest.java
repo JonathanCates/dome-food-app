09df4e80bc5ac5432d4b69e685e5418b8fc4ab33
unknown <jcate478@B103-013.ad.mtroyal.ca>
2016-02-10 14:40:59 -0700

    start

---
+++ app/src/test/java/com/example/jcate478/seat_suite/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.jcate478.seat_suite;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
